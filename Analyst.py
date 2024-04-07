6cc728af9cd2a0c00357f316ea1b2da9de9f8ff1
SSSavii <georgiii3512@mail.ru>
2024-04-07 17:48:06 +0700

    Обновил функции, добавил ещё одну и настроил вывод в эксель файл

--- Analyst.py
+++ Analyst.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 # Считываем данные из файла и сохраняем в словарь
 def read_data_from_file(filename):
     glyph_data = {}
@@ -15,7 +17,6 @@ def count_glyphs(glyph_data):
     for glyphs in glyph_data.values():
         for glyph in glyphs:
             glyph_count[glyph] = glyph_count.get(glyph, 0) + 1
-    # Сортировка и вывод с сохранением ведущих нулей
     sorted_glyph_count = {k: glyph_count[k] for k in sorted(glyph_count, key=lambda x: int(x))}
     return sorted_glyph_count
 
@@ -26,8 +27,7 @@ def find_repeated_glyphs(glyph_data):
         for glyph in set(glyphs):
             count = glyphs.count(glyph)
             if count > 1:
-                if glyph not in repeated_glyphs:
-                    repeated_glyphs[glyph] = (unicode, count)
+                repeated_glyphs[glyph] = (unicode, count)
     return repeated_glyphs
 
 # Функция для поиска всех повторяющихся паттернов графем
@@ -47,6 +47,20 @@ def find_all_repeated_patterns(glyph_data):
     repeated_patterns = {pair: unicodes for pair, unicodes in glyph_pairs.items() if len(unicodes) > 1}
     return repeated_patterns
 
+def count_glyphs_in_uni(glyph_data):
+    count_dict = {}
+    for glyphs in glyph_data.values():
+        glyph_count = len(glyphs)
+        count_dict[glyph_count] = count_dict.get(glyph_count, 0) + 1
+    return count_dict
+
+# Функция для вывода данных в Excel
+def output_to_excel(data_dict, filename='output.xlsx'):
+    with pd.ExcelWriter(filename, engine='openpyxl') as writer:
+        for sheet_name, data in data_dict.items():
+            df = pd.DataFrame(list(data.items()), columns=['Key', 'Value'])
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+
 # Использование функции чтения данных из файла
 glyph_data = read_data_from_file('data.txt')
 
@@ -54,8 +68,15 @@ glyph_data = read_data_from_file('data.txt')
 glyph_count = count_glyphs(glyph_data)
 repeated_glyphs = find_repeated_glyphs(glyph_data)
 all_repeated_patterns = find_all_repeated_patterns(glyph_data)
+glyphs_in_uni_count = count_glyphs_in_uni(glyph_data)
 
-# Вывод результатов
-print("Счётчик графем:", glyph_count)
-print("Повторяющиеся графемы:", repeated_glyphs)
-print("Все повторяющиеся паттерны:", all_repeated_patterns)
+# Создание словаря с данными для вывода в Excel
+data_to_output = {
+    'Счётчик графем': glyph_count,
+    'Повторяющиеся графемы': repeated_glyphs,
+    'Все повторяющиеся паттерны': all_repeated_patterns,
+    'Количество иероглифов по количеству графем': glyphs_in_uni_count
+}
+
+# Использование функции вывода данных в Excel
+output_to_excel(data_to_output)
